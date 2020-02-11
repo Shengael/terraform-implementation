73e0d9657ffba0d87c26849fb858b286bd01d2be
Luis Valdez <38981550+Shengael@users.noreply.github.com>
2020-02-11 15:23:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if ! dpkg-query -W -f='${Status}' git | grep "ok installed"; then
 fi
  
  
-git clone git@github.com:Shengael/terraform-implementation.git && cd terraform-implementation || return
+git clone https://github.com/Shengael/terraform-implementation.git && cd terraform-implementation || return
  
 curl https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip -o dl.zip && unzip dl.zip
 
